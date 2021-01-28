35e95f6915cba940aa4b5e6e12faed9c1ec5b33f
Vladyslav <vladsheln58@gmail.com>
2021-01-28 22:07:10 +0200

    test1

--- supportBot.py
+++ supportBot.py
@@ -10,16 +10,14 @@ keyboard1.row('Українська', 'English', 'Русский')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Оберіть дію:', reply_markup=keyboard1)
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
-    elif message.text.lower() == 'я тебя люблю':
-        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+    if message.text.lower() == 'технічна підтримка користувачів':
+        bot.send_message(message.chat.id, 'supportInfo')
+    if message.text.lower() == 'інформація про оплату':
+        bot.send_message(message.chat.id, 'paymentInfo')
 
 @bot.message_handler(content_types=['sticker'])
 def sticker_id(message):
