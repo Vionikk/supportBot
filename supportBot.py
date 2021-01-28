07bd8367a80f6f9b7bb763eb09ed24bf1ef285bb
Vionikk <vladsheln58@gmail.com>
2021-01-28 21:39:22 +0200

    test

--- supportBot.py
+++ supportBot.py
@@ -3,9 +3,27 @@ import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(content_types = ['text'])
-def func(message):
-    bot.send_message(message.chat.id, message.text)
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.add('Технічна підтримка користувачів')
+keyboard1.add('Інформація про оплату')
+keyboard1.row('Українська', 'English', 'Русский')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'я тебя люблю':
+        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    print(message)
 
 #RUN
 bot.polling(none_stop = True)
